ba2c834a0b2eb050456dfda2730d358e9c2b4b0e
Naman-2300 <namanp.2300@gmail.com>
2023-06-27 16:58:09 +0530

    tenth commit

---
+++ practice28.py
@@ -0,0 +1,7 @@
+class rectangle:
+    def area(width, height):
+        print(width*height)
+    
+class circle:
+    def area(radius):
+        print(3.14*radius*radius)
---
+++ practice29.py
@@ -0,0 +1,7 @@
+import practice28
+width, height = map(int, (input("enter kro: ").split()))
+radius = int(input("enter radius: "))
+
+practice28.rectangle.area(width, height)
+practice28.circle.area(radius)
+
